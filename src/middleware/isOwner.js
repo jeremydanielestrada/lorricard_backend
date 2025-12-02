796555b8d911865a92c598d78be2819d39aa59c3
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-12-02 19:17:14 +0800

    fix: isOwner middle implementation logic

--- src/middleware/isOwner.js
+++ src/middleware/isOwner.js
@@ -1,19 +1,26 @@
 import pool from "../config/db.js";
 
+///FOR OWNERSHIP VERIFICATION (eg.. Update ,Delete or View)
 export const isOwner = async (req, res, next) => {
-  const userId = req.user.id;
+  try {
+    const userId = req.user.id;
+    const folderId = req.params.folderId; // Get specific folder ID
 
-  const result = await pool.query("SELECT * FROM folders WHERE user_id = $1", [
-    userId,
-  ]);
+    const result = await pool.query(
+      "SELECT user_id FROM folders WHERE id = $1",
+      [folderId]
+    );
 
-  if (result.rows.length === 0) {
-    return res.status(404).json({ message: "Folder not found" });
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Folder not found" });
+    }
+
+    if (result.rows[0].user_id !== userId) {
+      return res.status(403).json({ message: "Not allowed" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: "Server error" });
   }
-
-  if (result.rows[0].user_id !== userId) {
-    return res.status(403).json({ message: "Not allowed" });
-  }
-
-  next();
 };
--- src/routes/folder.js
+++ src/routes/folder.js
@@ -3,13 +3,12 @@ import {
   getAllFoldersByOwner,
   createFolder,
 } from "../controller/folderController.js";
-import { isOwner } from "../middleware/isOwner.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 export const folderRouter = express.Router();
 
 //View Folder
-folderRouter.get("/view", protect, isOwner, getAllFoldersByOwner);
+folderRouter.get("/view", protect, getAllFoldersByOwner);
 
 //Create Folder
 folderRouter.post("/create", protect, createFolder);
