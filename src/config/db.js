5fed036222727f67b4fc092253c7be7e05433f38
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-12-21 20:06:26 +0800

    feat: updated pool with connectionString on neon db

--- src/config/db.js
+++ src/config/db.js
@@ -5,8 +5,10 @@ dotenv.config();
 
 //DB attirbutes
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:rejectUnauthorized: process.env.NODE_ENV === "production" ? {rejectUnauthorized: false} : false,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT,  
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
