808940b97719c912b58ab01f18517806b0947d03
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-12-21 20:50:21 +0800

    fix: typo on fileUploadMiddleware

--- src/routes/flashCards.js
+++ src/routes/flashCards.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { upload } from "../middleware/FileUploaMiddleware.js";
+import { upload } from "../middleware/FileUploadMiddleware.js";
 import {
   getAllFlashCardsByFolderId,
   createFlashCardsFromDocument,
